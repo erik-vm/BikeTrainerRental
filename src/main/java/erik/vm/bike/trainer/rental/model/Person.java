62bcbf1962a36131ae4cfce888111e59eedc270c
Erik Vainumäe <erik.vainumae@gmail.com>
2025-01-21 22:58:22 +0200

    created person and user entities

---
+++ src/main/java/erik/vm/bike/trainer/rental/model/Person.java
@@ -0,0 +1,40 @@
+package erik.vm.bike.trainer.rental.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "person_id")
+    private UUID personId;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "Firstname has to be between 1-50 characters.")
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "Lastname has to be between 1-50 characters.")
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
+    private User user;
+}
---
+++ src/main/java/erik/vm/bike/trainer/rental/model/User.java
@@ -0,0 +1,36 @@
+package erik.vm.bike.trainer.rental.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @NotNull
+    @NotBlank(message = "Cant leave username blank!")
+    @Column(unique = true)
+    @Size(min = 6, max = 20, message = "Username has to be between 6-20 characters.")
+    private String username;
+
+    @NotNull
+    @NotBlank(message = "Cant leave password blank!")
+    @Size(min = 6, max = 20, message = "Password has to be between 6-20 characters.")
+    private String password;
+
+    @OneToOne
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+}
