c518e8e60797a3579286ea9511c76635fc2cc5fe
Erik Vainumäe <erik.vainumae@gmail.com>
2025-01-21 22:36:37 +0200

    initial

---
+++ src/main/java/erik/vm/bike/trainer/rental/BikeTrainerRentalApplication.java
@@ -0,0 +1,13 @@
+package erik.vm.bike.trainer.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeTrainerRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeTrainerRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/erik/vm/bike/trainer/rental/BikeTrainerRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package erik.vm.bike.trainer.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeTrainerRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
